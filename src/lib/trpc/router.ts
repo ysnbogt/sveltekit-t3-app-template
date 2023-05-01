aa6a38c02a4258231eae306ec1b58642ad12c2de
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2023-05-02 02:35:39 +0900

    update: remove comment out

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -1,16 +1,3 @@
-// import type { Context } from "$lib/trpc/context"
-// import { initTRPC } from "@trpc/server"
-
-// export const t = initTRPC.context<Context>().create()
-
-// export const router = t.router({
-//   greeting: t.procedure.query(async () => {
-//     return `Hello tRPC v10 @ ${new Date().toLocaleTimeString()}`
-//   }),
-// })
-
-// export type Router = typeof router
-
 import { users } from "$lib/trpc/routes/users"
 import { t } from "$lib/trpc/t"
 import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server"
