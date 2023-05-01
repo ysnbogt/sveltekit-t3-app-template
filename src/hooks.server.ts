3a635923ee54b42368b8457ca6b14d917d448163
ysnbogt <ysnbogt@gmail.com>
2023-05-02 07:31:55 +0900

    update

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,3 +1,4 @@
+import Discord from "@auth/core/providers/discord"
 import GitHub from "@auth/core/providers/github"
 import { SvelteKitAuth } from "@auth/sveltekit"
 import { sequence } from "@sveltejs/kit/hooks"
@@ -5,7 +6,12 @@ import { createTRPCHandle } from "trpc-sveltekit"
 
 import type { Handle } from "@sveltejs/kit"
 
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "$env/static/private"
+import {
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET,
+  DISCORD_CLIENT_ID,
+  DISCORD_CLIENT_SECRET,
+} from "$env/static/private"
 import { createContext } from "$lib/trpc/context"
 import { router } from "$lib/trpc/router"
 
@@ -15,6 +21,10 @@ const authHandle = SvelteKitAuth({
     GitHub({
       clientId: GITHUB_CLIENT_ID,
       clientSecret: GITHUB_CLIENT_SECRET,
+    }) as any,
+    Discord({
+      clientId: DISCORD_CLIENT_ID,
+      clientSecret: DISCORD_CLIENT_SECRET,
     }) as any,
   ],
 })
